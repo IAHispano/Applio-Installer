01bfb9532bcb1f56145b53254565665da874b039
bygimenezz <davidalbal123@gmail.com>
2023-09-20 15:25:17 +0200

    pre-installed dependencies (runtime)

--- install_Applio.bat
+++ install_Applio.bat
@@ -8,19 +8,15 @@ set "runtime_scripts=%cd%\%repoFolder%\runtime\Scripts"
 set "URL_BASE=https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main"
 set "URL_EXTRA=https://huggingface.co/IAHispano/applio/resolve/main"
 
+cls
 for /f "delims=: tokens=*" %%A in ('findstr /b ":::" "%~f0"') do @echo(%%A
 echo.
 
-echo Cloning the repository...
 git clone %repoUrl% %repoFolder%
 cd %repoFolder%
 echo.
 cls
 
-echo Proceeding to download the models...
-echo.
-
-echo Downloading the "pretrained" folder...
 cd "assets/pretrained/"
 curl -LJO "%URL_BASE%/pretrained/D32k.pth"
 curl -LJO "%URL_BASE%/pretrained/D40k.pth"
@@ -38,7 +34,6 @@ cd ".."
 echo.
 cls
 
-echo Downloading the "pretrained_v2" folder...
 cd "pretrained_v2"
 curl -LJO "%URL_BASE%/pretrained_v2/D32k.pth"
 curl -LJO "%URL_BASE%/pretrained_v2/D40k.pth"
@@ -56,7 +51,6 @@ cd ".."
 echo.
 cls
 
-echo Downloading the "uvr5_weights" folder...
 cd "uvr5_weights"
 curl -LJO "%URL_BASE%/uvr5_weights/HP2_all_vocals.pth"
 curl -LJO "%URL_BASE%/uvr5_weights/HP3_all_vocals.pth"
@@ -68,14 +62,12 @@ cd ".."
 echo.
 cls
 
-echo Downloading the rmvpe.pt file...
 cd "rmvpe"
 curl -LJO "%URL_BASE%/rmvpe.pt"
 echo.
 cls
 cd ".."
 
-echo Downloading the hubert_base.pt file...
 cd "hubert"
 curl -LJO "%URL_BASE%/hubert_base.pt"
 echo.
@@ -83,9 +75,9 @@ cls
 cd ".."
 cd ".."
 
-echo Downloading the ffmpeg.exe and ffprobe.exe file...
 curl -LJO "%URL_BASE%/ffmpeg.exe"
 curl -LJO "%URL_BASE%/ffprobe.exe"
+cls
 
 @echo off
 setlocal
@@ -93,32 +85,23 @@ setlocal
 echo Downloading torchcrepe
 mkdir temp_torchcrepe
 echo.
+cls
 
-echo Clone the GitHub repository to the temporary directory
 git clone --depth 1 https://github.com/maxrmorrison/torchcrepe.git temp_torchcrepe
-
-echo Copy the "torchcrepe" folder and its contents to the current directory
 robocopy "temp_torchcrepe\torchcrepe" ".\torchcrepe" /E
-echo.
-
-echo Remove the temporary directory
 rmdir /s /q temp_torchcrepe
 echo.
-
-echo Torchcrepe downloaded successfully!
 cls
 
 echo Installing dependencies...
 cls
-pip install -r assets/requirements/requirements.txt
+curl -LJO "%URL_EXTRA%/runtime.zip"
 echo.
-pip uninstall torch torchvision torchaudio -y
+echo Extracting the runtime.zip file...
+powershell -command "& { Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory('runtime.zip', '%principal%') }"
 echo.
-pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
-echo.
-echo.
+del runtime.zip
 cls
-echo Dependencies installed!
 echo.
 goto dependenciesFinished
 )
